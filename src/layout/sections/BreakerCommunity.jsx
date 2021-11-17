be050a9fc4a320d638f332f59617643109d271b6
Joroh <mohamedaitsimhand@gmail.com>
2021-11-17 19:50:48 +0100

    Breacker Community section ready

--- src/layout/sections/BreakerCommunity.jsx
+++ src/layout/sections/BreakerCommunity.jsx
@@ -1,10 +1,61 @@
 import styled from "styled-components";
 import { Texture } from "../../components/Texture";
 
+import Logo from "../../img/logo/brand-black.svg";
+
 const BreakerCommunity = () => {
-  return <BreakerCommunityStyle className="container">BreakerCommunity</BreakerCommunityStyle>;
+  return (
+    <BreakerCommunityStyle>
+      <Texture />
+      <div className="background" />
+      <div className="row">
+        <img src={Logo} alt="" />
+        <div>
+          <h4>Bushido</h4>
+          <h5>7077</h5>
+          <a className="btn" href="/">
+            join our community
+          </a>
+        </div>
+      </div>
+    </BreakerCommunityStyle>
+  );
 };
 
 export default BreakerCommunity;
 
-const BreakerCommunityStyle = styled.section``;
+const BreakerCommunityStyle = styled.section`
+  padding: 50px 15px;
+  > div:first-of-type {
+    ::after {
+      opacity: 0.1;
+    }
+  }
+  .background {
+    background: #ffe600;
+    position: absolute;
+    inset: 0;
+    z-index: -2;
+  }
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  h4,
+  h5 {
+    color: #14191a;
+    margin-bottom: 0;
+    line-height: 1.2;
+  }
+  a {
+    margin-top: 10px;
+  }
+
+  @media (max-width: 567px) {
+    .row {
+      flex-direction: column;
+      text-align: center;
+    }
+  }
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
         {/* <ShowcaseTop /> */}
         {/* <ShowcaseReel /> */}
 
-        <Roadmap />
+        {/* <Roadmap /> */}
         <BreakerCommunity />
         <MeetTheTeam />
         <FAQ />
